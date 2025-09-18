b8e9561cd12104ec846f85605539118ca70c81de
Ryan de Menezes <ryan.menezes@digitalindustry.com.br>
2025-09-18 15:31:48 -0300

    Finalizando os recursos da controller de Paciente

--- src/main/java/pdev/com/agenda/api/controllers/PacienteController.java
+++ src/main/java/pdev/com/agenda/api/controllers/PacienteController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +35,30 @@ public class PacienteController {
         var pacientes = pacienteService.findAll();
 
         return ResponseEntity.ok(pacientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> findById(@PathVariable Long id) {
+        var response = pacienteService.findById(id);
+
+        if (response.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(response.get());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> update(@RequestBody Paciente data) {
+        var paciente = pacienteService.save(data);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(paciente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Paciente> delete(@PathVariable Long id) {
+        pacienteService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
